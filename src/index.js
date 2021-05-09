7080bdc9a40c03b8d405998f90b8dce3ea4fe69b
craftycram <github@craftycram.net>
2021-05-09 14:27:11 +0200

    added led module arduino code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { default: axios } = require("axios");
+const ws = require('ws');
 
 let ownerCount = 0;
 let peopleCount = 0;
@@ -6,6 +7,28 @@ let peopleCount = 0;
 const host = 'http://localhost';
 const portFaces = 8001;
 const portIdentify = 8000;
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 81 });
+
+let wssClients = [];
+
+wss.on('connection', function connection(ws) {
+  wssClients.push(ws);
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  ws.on('close', () => {
+    console.log('closed. removing client');
+    wssClients.filter((e) => e === wss);
+  });
+  ws.on('error', () => {
+    console.log('errored. removing client');
+    wssClients.filter((e) => e === wss);
+  });
+  ws.send('something');
+});
 
 setInterval(() => {
   axios.get(`${host}:${portIdentify}/data`).then((res) => {
@@ -21,11 +44,21 @@ setInterval(() => {
   });
   if (ownerCount === 0 && peopleCount > 0) {
     console.log('Alarm! Unbekannte Person');
+    setLED(3);
   } else if (ownerCount > 0 && peopleCount > 1) {
+    setLED(2);
     console.log('Besuch! Hoffentlich alle negativ.');
   } else if (ownerCount > 0 && peopleCount > 0) {
+    setLED(1);
     console.log('Howdy Howy.');
   } else {
+    setLED(0);
     console.log('Keiner Zuhause!');
   }
 }, 1000);
+
+function setLED(state) {
+  wssClients.forEach((c) => {
+    c.send(state);
+  });
+}
